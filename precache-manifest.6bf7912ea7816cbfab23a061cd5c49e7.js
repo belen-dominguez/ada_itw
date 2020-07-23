ad5bafdc5da42727634e131f0365a6d84165fbe5
belen-dominguez <belen.dominguez.sigler@outlook.com>
2020-07-23 09:27:27 -0300

    Updates

---
+++ precache-manifest.6bf7912ea7816cbfab23a061cd5c49e7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240b841948d09200cc3fad98d1a8fe9c",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/index.html"
+  },
+  {
+    "revision": "fc9d053e636630c9b68b",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/css/main.dc46ee2e.chunk.css"
+  },
+  {
+    "revision": "518c55e75a13d12653d8",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/js/2.adc62648.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/js/2.adc62648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9d053e636630c9b68b",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/js/main.136d39f1.chunk.js"
+  },
+  {
+    "revision": "53cc80d23d432974766b",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/js/runtime-main.85a7c761.js"
+  },
+  {
+    "revision": "8af3ced1f6cb605890f0826aec5f2f7e",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/01.8af3ced1.jpg"
+  },
+  {
+    "revision": "4d39d9d821fb791b66f1e8959563de75",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/02.4d39d9d8.jpg"
+  },
+  {
+    "revision": "0f13be35fa39528916cd66eb27e8d58a",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/03.0f13be35.jpg"
+  },
+  {
+    "revision": "6527d1c1c6275d4c801fa36e7f6cc68f",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/04.6527d1c1.jpg"
+  },
+  {
+    "revision": "383ca1f9f298074ce26180acba0fff4e",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/05.383ca1f9.jpg"
+  },
+  {
+    "revision": "b32e16acf7727fd9d32f958d4ca26224",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/06.b32e16ac.jpg"
+  },
+  {
+    "revision": "12325b1994250e6c6d127034bdb79d68",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/07.12325b19.jpg"
+  },
+  {
+    "revision": "d22712c77383877dad1881fd46875de7",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/08.d22712c7.jpg"
+  },
+  {
+    "revision": "bd78f89705c7183bcb785532ad656032",
+    "url": "/belen-dominguez/ada_itw/22-Recipe_card/static/media/09.bd78f897.jpg"
+  }
+]);
